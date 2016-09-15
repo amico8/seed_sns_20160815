8ced726bbf9e2150663bc6999e22f6c0f78579e6
amico <amico8amico@gmail.com>
2016-09-15 13:33:28 +0800

    画像再指定のメッセージ出力

--- join/index.php
+++ join/index.php
@@ -157,6 +157,9 @@
               <?php if (isset($error['picture_path']) && $error['picture_path'] == 'type'): ?>
                 <p class="error">* 写真などは「.gif」「.jpg」「.png」の画像を指定してください。</p>
               <?php endif; ?>
+              <?php if (!empty($error)): ?>
+                <p class="error">* 恐れ入りますが、画像を改めて指定してください。</p>
+              <?php endif; ?>
             </div>
           </div>
 
