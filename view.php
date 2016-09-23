e498d8bfc7cc24047d63f38e0c5c6868d1f0891c
amico <amico8amico@gmail.com>
2016-09-23 14:29:15 +0800

    view.phpにも削除処理を反映

--- view.php
+++ view.php
@@ -78,7 +78,9 @@
           </p>
           <p class="day">
             <?php echo htmlspecialchars($tweet['created'], ENT_QUOTES, 'UTF-8'); ?>
-            [<a href="#" style="color: #F33;">削除</a>]
+            <?php if ($_SESSION['id'] == $tweet['member_id']): ?>
+            [<a href="delete.php?tweet_id=<?php echo htmlspecialchars($tweet['tweet_id'], ENT_QUOTES, 'UTF-8'); ?>" style="color: #F33;">削除</a>]
+            <?php endif; ?>
           </p>
         </div>
       <?php else: ?>
